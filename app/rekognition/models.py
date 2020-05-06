6ad45a1b0820903c00cfe936ea3719d1de7cbc6f
dudar99 <dudaryourko@gmail.com>
2020-05-06 14:39:49 +0300

    add photo decoder and resolve photo disapreing issue

--- app/rekognition/models.py
+++ app/rekognition/models.py
@@ -12,4 +12,4 @@ class Person(models.Model):
     last_name = models.CharField(max_length=30)
     face_id = models.CharField(max_length=100)
     image_path = models.CharField(max_length=100)
-    collection_id = models.ManyToManyField(Collection)
+    collection_id_id = models.ForeignKey(Collection, on_delete=models.CASCADE, default=1)
--- app/rekognition/views.py
+++ app/rekognition/views.py
@@ -1,3 +1,4 @@
+import base64
 import os
 
 import boto3
@@ -12,7 +13,7 @@ DEFAULT_IMG_LOCAL_PATH = '/home/dudar99/Pictures'
 DEFAULT_BUCKET = "videos-sample-1"
 rekognition = boto3.client('rekognition', region_name='us-east-1')
 s3 = boto3.client('s3')
-lambda_ = boto3.client('lambda')
+
 
 def add_face_to_folder(bucket, path, photo_name):
     name = os.path.join(path, photo_name)
@@ -57,22 +58,62 @@ def collection(request):
     return HttpResponse(template.render(context, request))
 
 
-def collection_faces(request, id):
-    template = loader.get_template('rekognition/collection_faces.html')
-    collection = Collection.objects.filter(id=id).first()
-    path_to_collection = os.path.join('faces/', collection.collection_id)
-    context = {
-        'faces': Person.objects.filter(collection_id=id),
+def get_base64_image(person):
+    obj = s3.get_object(Bucket=DEFAULT_BUCKET, Key=person.image_path)
+    body = obj.get('Body').read()
+    base64_encoded_data = base64.b64encode(body)
+    base64_message = base64_encoded_data.decode('utf-8')
+    return base64_message
+
+
+def create_context(id):
+    return {
+        'faces': [{'person': person, 'img': get_base64_image(person)} for person in
+                  Person.objects.filter(collection_id_id=id)],
         'collection_id': id
     }
+
+
+def collection_faces(request, id):
+    errors = []
+    collection = Collection.objects.filter(id=id).first()
+    path_to_collection = os.path.join('faces/', collection.collection_id)
+
     if request.method == 'POST':
         first_name = request.POST.get('first_name')
         last_name = request.POST.get('last_name')
         img = request.POST.get('img')
         img_path = os.path.join(DEFAULT_IMG_LOCAL_PATH, img)
+
         with open(img_path, "rb") as f:
-            s3.upload_fileobj(f, DEFAULT_BUCKET, os.path.join(path_to_collection, img))
-        lambda_
+            key = os.path.join(path_to_collection, img)
+            s3.upload_fileobj(f, DEFAULT_BUCKET, key)
+
+        response = rekognition.index_faces(
+            CollectionId=collection.collection_id,
+            Image={
+                'S3Object': {
+                    'Bucket': DEFAULT_BUCKET,
+                    'Name': key,
+                }
+            },
+            MaxFaces=1,
+        )
+        face_id = response.get('FaceRecords', [{}])[0].get('Face', {}).get('FaceId')
+        face_metadata = response.get('FaceRecords', [{}])[0].get('FaceDetail', {})
+        if not face_id:
+            errors.append("this image does not contain eny faces")
+        person = Person(
+            collection_id_id=collection,
+            face_id=face_id,
+            first_name=first_name,
+            last_name=last_name,
+            image_path=key
+        )
+        person.save()
+
+    template = loader.get_template('rekognition/collection_faces.html')
+    context = create_context(id)
 
     return HttpResponse(template.render(context, request))
 
